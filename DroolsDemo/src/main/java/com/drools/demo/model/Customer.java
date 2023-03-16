fab39bbb7b1a55dd49afab2732935e0213f383a4
coder <coder@coder-vk00933780-ug-dev-workspace-vk>
2023-03-16 10:15:55 +0000

    Updated

--- DroolsDemo/src/main/java/com/drools/demo/model/Customer.java
+++ DroolsDemo/src/main/java/com/drools/demo/model/Customer.java
@@ -14,27 +14,27 @@ public class Customer {
 	private int customerId;
 	private String customerFirstName;
 	private String customerLastName;
-	private String customerMiddleName;
-	private String preferredName;
+	//private String customerMiddleName;
+	//private String preferredName;
 	private String primaryContactNumber;
-	private String secondaryContactNumner;
+	//private String secondaryContactNumner;
 	private String address;
 	private String emailId;
 	private int age;
 	private String dateOfBirth;
 	private String gender;
-	private String race;
+	/*private String race;
 	private String religion;
 	private String maritalStatus;
 	private String ethnicity;
-	private String hobbies;
+	private String hobbies; */
 	private String category;
 	private String personality;
 	private List<String> preferences;
-	private int primaryGuestId;
+	/*private int primaryGuestId;
 	private String primaryGuestRelationship;
 	private String createdDate;
-	private String updatedDated;
+	private String updatedDated; */
 	private List<Gift> gifts;
 
 }
